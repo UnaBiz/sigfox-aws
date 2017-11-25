03dba0a5a2679e6da24ec0cce7015fdff0589ed6
lupyuen-unabiz <ly.lee@unabiz.com>
2017-11-25 11:45:12 +0800

    Changed handler to index.main

--- sigfoxCallback/index.js
+++ sigfoxCallback/index.js
@@ -2,6 +2,7 @@
 //  Copy and paste the entire contents of this file into a Lambda Function
 //  Name: sigfoxCallback
 //  Runtime: Node.js 6.10
+//  Handler: index.main
 //  Memory: 512 MB
 //  Timeout: 1 min
 //  Existing Role: lambda_iot (defined according to ../policy/LambdaExecuteIoTUpdate.json)
