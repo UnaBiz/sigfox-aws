a28c7c9d2d0ae1f1adb5d4d5d1d12cb797d1e441
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2017-11-26 12:08:56 +0800

    Streamlining autoinstall

--- test-autoinstall.js
+++ test-autoinstall.js
@@ -44,7 +44,13 @@ function autoinstall(event, context, callback) {
   return null;
 }
 const wrapper = {};  //  The single reused wrapper instance (initially empty) for invoking the module functions.
-exports.main = isAWS ? autoinstall : null; //  exports.main is the AWS Lambda and Google Cloud Function startup function.
+if (isAWS) exports.main = autoinstall; //  exports.main is the AWS Lambda and Google Cloud Function startup function.
+else {  //  For Google Cloud, select the 2-para or 1-para version of main() depending on the call mode: HTTP or PubSub Queue.
+  if (!wrapper.main) Object.assign(wrapper, wrap(package_json));
+  const mainFunc = wrapper.main.bind(wrapper);
+  if (process.env.FUNCTION_TRIGGER_TYPE === 'HTTP_TRIGGER') exports.main = (req0, res0) => mainFunc(req0, res0);
+  else exports.main = event0 => mainFunc(event0);
+}
 
 //  //////////////////////////////////////////////////////////////////////////////////// endregion
 
