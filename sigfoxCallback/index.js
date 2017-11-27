ac415aadb0d96d4833f4488fead066fa60e7a9cf
lupyuen-unabiz <ly.lee@unabiz.com>
2017-11-27 21:15:17 +0800

    Tested new sigfoxCallback

--- sigfoxCallback/index.js
+++
@@ -1,4 +0,0 @@
-//  sigfoxCallback is a common Google Cloud and AWS Lambda function defined in sigfox-iot-cloud
-/* eslint-disable import/no-unresolved */
-
-module.exports = require('sigfox-iot-cloud/sigfoxCallback');
---
+++ sigfoxCallback/lambda.js
@@ -0,0 +1,65 @@
+//  region Introduction
+//  sigfoxCallback Installation Instructions:
+//  Copy and paste the entire contents of this file into a Lambda Function
+//  Name: sigfoxCallback
+//  Runtime: Node.js 6.10
+//  Handler: index.main
+//  Memory: 512 MB
+//  Timeout: 1 min
+//  Existing Role: lambda_iot Role, which has the LambdaExecuteIoTUpdate Policy
+//    (defined in ../policy/LambdaExecuteIoTUpdate.json)
+//  Debugging: Enable active tracing
+//  Environment Variables:
+//    NODE_ENV=production
+//    AUTOINSTALL_DEPENDENCY=sigfox-iot-cloud/sigfoxCallback
+
+//  Create an API Gateway named sigfoxGateway (New API, Edge optimised)
+//  In the sigfoxCallback configuration, add a trigger from sigfoxGateway
+//  API Name: sigfoxGateway
+//  Deployment Stage: prod
+//  Security: Open
+//  Method: ANY
+//  Resource path: /sigfoxCallback
+//  Authorization: NONE
+
+//  Invoke URL should look like:
+//  https://8xcb9t7mpj.execute-api.ap-southeast-1.amazonaws.com/prod/sigfoxCallback
+//  For testing: See ./test/testEvent.json for test event.
+//  The "time" field should be set to number of seconds
+//  since 1970 Jan 1 UTC (e.g. 1511614827).  Use Chrome console to compute: Date.now() / 1000
+
+//  Cloud Function sigfoxCallback is exposed as a HTTPS service
+//  that Sigfox Cloud will callback when delivering a Sigfox message.
+//  For AWS: We insert the Sigfox message into the message queue sigfox.received
+//  For Google Cloud: We insert the Sigfox message into these message queues:
+//  (1) sigfox.devices.all (the queue for all devices)
+//  (2) sigfox.devices.<deviceID> (the device specific queue)
+//  (3) sigfox.types.<deviceType> (the specific device type e.g. gps)
+
+//  We will return the HTTPS response immediately to Sigfox Cloud while
+//  the processing of the Sigfox continues with other Google Cloud Functions.
+//  This code is critical, all changes must be reviewed.  It must be
+//  kept as simple as possible to reduce the chance of failure.
+
+//  We set the wrap function and package_json to null indicate that AutoInstall
+//  should install the dependency from environment variable AUTOINSTALL_DEPENDENCY (see above).
+const wrap = null; // eslint-disable-next-line camelcase
+const package_json = null;
+
+//  //////////////////////////////////////////////////////////////////////////////////// endregion
+//  region Standard Code for AutoInstall Startup Function.  Do not modify.  https://github.com/UnaBiz/sigfox-iot-cloud/blob/master/autoinstall.js
+/* eslint-disable camelcase,no-unused-vars,import/no-absolute-path,import/no-unresolved,no-use-before-define,global-require,max-len,no-tabs,brace-style,import/no-extraneous-dependencies */
+const wrapper = {};  //  The single reused wrapper instance (initially empty) for invoking the module functions.
+exports.main = process.env.FUNCTION_NAME ? require('sigfox-gcloud/main').getMainFunction(wrapper, wrap, package_json)  //  Google Cloud.
+  : (event, context, callback) => { //  exports.main is the startup function for AWS Lambda and Google Cloud Function.
+    //  When AWS starts our Lambda function, we load the autoinstall script from GitHub to install any NPM dependencies.
+    //  For first run, install the dependencies specified in package_json and proceed to next step.
+    //  For future runs, just execute the wrapper function with the event, context, callback parameters.
+    const afterExec = error => error ? callback(error, 'AutoInstall Failed')
+      : require('/tmp/autoinstall').installAndRunWrapper(event, context, callback,
+        package_json, __filename, wrapper, wrap);
+    if (require('fs').existsSync('/tmp/autoinstall.js')) return afterExec(null);  //  Already downloaded.
+    const cmd = 'curl -s -S -o /tmp/autoinstall.js https://raw.githubusercontent.com/UnaBiz/sigfox-iot-cloud/master/autoinstall.js';
+    const child = require('child_process').exec(cmd, { maxBuffer: 1024 * 500 }, afterExec);
+    child.stdout.on('data', console.log); child.stderr.on('data', console.error); return null; };
+//  //////////////////////////////////////////////////////////////////////////////////// endregion
