8b2de4807314b47b413afdba4cf6ecc53f8c092c
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2017-11-29 15:13:18 +0800

    Handle task function as export

---
+++ main/index.js
@@ -0,0 +1,19 @@
+//  Helper for main function
+/*  eslint-disable max-len,camelcase,import/no-extraneous-dependencies,import/no-unresolved,global-require,import/newline-after-import */
+
+process.on('uncaughtException', err => console.error('uncaughtException', err.message, err.stack));  //  Display uncaught exceptions.
+process.on('unhandledRejection', (reason, p) => console.error('unhandledRejection', reason, p));  //  Display uncaught promises.
+
+const scloud = require('sigfox-aws');
+
+function getMainFunction(wrapper, wrap, package_json) {
+  if (!wrapper || !wrap) throw new Error('Missing wrapper or wrap function');
+  if (!wrapper.main && !wrapper.task) Object.assign(wrapper, wrap(scloud, package_json));
+  const mainFunc = wrapper.main ? wrapper.main.bind(wrapper) : scloud.main.bind(wrapper);
+  const taskFunc = wrapper.task ? wrapper.task.bind(wrapper) : null;
+  return (event, context, callback) => mainFunc(event, context, callback, taskFunc);
+}
+
+module.exports = {
+  getMainFunction,
+};
